8ddc5fafd134ba2b444c220e39b15200e3df1467
Reverie Wisp <contact@reveriewisp.com>
2019-06-22 15:51:19 -0700

    = allows startup

---
+++ app.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('main/index.html')
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+// Startup app on ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
